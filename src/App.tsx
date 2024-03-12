3c6166b64d1807059b802fcb6449d95397322ed9
Tiago Gomes <tidgomes@gmail.com>
2024-03-12 11:57:29 -0300

    validate email and password fields using zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 import './styles/global.css'
 
+const createUserFormSchema = z.object({
+  email: z
+    .string()
+    .min(1, 'E-mail é requerido')
+    .email('Formato de e-mail inválido'),
+  password: z.string().min(6, 'A senha precisa de no mínimo 6 caracteres')
+})
+
+type CreateUserFormSchema = z.infer<typeof createUserFormSchema>
+
 function App() {
+  const [output, setOutPut] = useState('')
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<CreateUserFormSchema>({
+    resolver: zodResolver(createUserFormSchema)
+  })
+
+  const createUser = (data: any) => {
+    console.log(data)
+    setOutPut(JSON.stringify(data, null, 4))
+  }
+
   return (
-    <main className='h-screen bg-zinc-50 flex items-center justify-center'>
-      <form className='flex flex-col gap-4 w-full max-w-xs'>
+    <main className='h-screen bg-zinc-50 flex flex-col gap-10 items-center justify-center'>
+      <form
+        className='flex flex-col gap-4 w-full max-w-xs'
+        onSubmit={handleSubmit(createUser)}
+      >
         <div className='flex flex-col gap-1'>
           <label htmlFor='email'>E-mail</label>
           <input
             className='border border-zinc-200 shadow-sm rounded h10 px-3'
             type='email'
-            name='email'
             id='email'
+            {...register('email')}
           />
+          {errors.email && <span>{errors.email.message}</span>}
         </div>
         <div className='flex flex-col gap-1'>
           <label htmlFor='password'>Senha</label>
           <input
             className='border border-zinc-200 shadow-sm rounded h10 px-3'
             type='password'
-            name='password'
             id='password'
+            {...register('password')}
           />
+          {errors.password && <span>{errors.password.message}</span>}
         </div>
 
         <button
@@ -30,6 +63,7 @@ function App() {
           Salvar
         </button>
       </form>
+      <pre>{output}</pre>
     </main>
   )
 }
