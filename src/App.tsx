ab150e8a9d4020f3afb4229af6b0788f84a38e27
Tiago Gomes <tidgomes@gmail.com>
2024-03-12 11:20:41 -0300

    create basic form format

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
 import './styles/global.css'
 
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <main className='h-screen bg-zinc-50 flex items-center justify-center'>
+      <form className='flex flex-col gap-4 w-full max-w-xs'>
+        <div className='flex flex-col gap-1'>
+          <label htmlFor='email'>E-mail</label>
+          <input
+            className='border border-zinc-200 shadow-sm rounded h10 px-3'
+            type='email'
+            name='email'
+            id='email'
+          />
+        </div>
+        <div className='flex flex-col gap-1'>
+          <label htmlFor='password'>Senha</label>
+          <input
+            className='border border-zinc-200 shadow-sm rounded h10 px-3'
+            type='password'
+            name='password'
+            id='password'
+          />
+        </div>
+
+        <button
+          className='bg-emerald-500 rounded font-semibold text-white h-10 hover:bg-emerald-600'
+          type='submit'
+        >
+          Salvar
+        </button>
+      </form>
+    </main>
+  )
 }
 
 export default App
